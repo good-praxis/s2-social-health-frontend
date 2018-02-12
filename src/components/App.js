b27f26bbd729e14a7819a568610894ea807dc135
Unknown <gabriela.sikora@code.berlin>
2018-02-12 13:59:48 +0100

    Changed app.js to access legacy
    It's filled with errors

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,22 @@
-import React from 'react'
-import Construction from "./Construction"
+import React, { Component } from 'react';
+import '../../legacy/chat/src/App.css';
+import { Sidebar } from "../../legacy/chat/src/containers/Sidebar";
+import { MessagesList } from "../../legacy/chat/src/containers/MessagesList";
+import { AddMessage } from "../../legacy/chat/src/containers/AddMessage"
 
-const App = () => (
-    <Construction/>
-)
 
-export default App
+class App extends Component {
+  render() {
+    return (
+      <div id="container">
+        <Sidebar />
+        <section id="main">
+          <MessagesList />
+          <AddMessage />
+        </section>
+      </div>
+    )
+  }
+}
+
+export default App;
