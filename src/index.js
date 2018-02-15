d548b4dc04b6deafbaf42ced9b6f32d4e69f6065
Unknown <gabriela.sikora@code.berlin>
2018-02-15 15:25:40 +0100

    Added redux saga and rearranged imports in index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from "./components/App";
-
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
+import createSagaMiddleware from 'redux-saga'
 
+import App from "./components/App";
 import registerServiceWorker from './registerServiceWorker'
 import chat from './reducers'
 import { addUser } from './actions'
 import { setupSocket } from './sockets'
+
 
 const store = createStore(chat)
 
