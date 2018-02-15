389ecb467888724ae33ff5ec361c5fc3358fe2ab
Unknown <gabriela.sikora@code.berlin>
2018-02-15 14:34:06 +0100

    Added cases of ADD_USER and ADD_MESSAGE to server setup

--- server/app.js
+++ server/app.js
@@ -11,3 +11,34 @@ const broadcast = (data, ws) => {
         }
     })
 }
+
+wss.on('connection', (ws) => {
+    let index
+    ws.on('message', (message) => {
+        const data = JSON.parse(message)
+        switch (data.type){
+            case 'ADD_USER' : {
+                index = users.length
+                users.push({ name: data.name, id: index + 1})
+                ws.send(JSON.stringify({
+                    type: 'USERS_LIST',
+                    users
+                }))
+                broadcast({
+                    type: 'USERS_LIST',
+                    users
+                }, ws)
+                break
+            }
+            case 'ADD_MESSAGE':
+                broadcast({
+                    type: 'ADD_MESSAGE',
+                    message: data.message,
+                    author: data.author
+                }, ws)
+                break
+                default:
+                break
+        }
+    })
+}
