7cb5f063d7b0e0afb045f6907fef62508a7a7807
Darian <thehelljelp@gmail.com>
2018-02-26 12:45:41 +0100

    resetting master to commit

--- src/components/chat/Message.js
+++ src/components/chat/Message.js
@@ -4,6 +4,11 @@ import propTypes from 'prop-types'
 const Message = ({message, author, timestamp, date}) => (
     <div>
         <p>
+            <img src="//res.cloudinary.com/teepublic/image/private/s--QOCrZKv---/t_Preview/b_rgb:c8e0ec,c_limit,f_auto,h_313,q_90,w_313/v1506740646/production/designs/1939321_0" 
+            alt="107378" 
+            className="circle" 
+            height="32" 
+            width="32"/>
             <i>{author}</i>: {message}
         </p>
         <p>
