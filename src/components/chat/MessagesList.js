a82543a17be79dcabb9909c64ec7ace78dc2f4e4
Darian <thehelljelp@gmail.com>
2018-03-05 11:33:51 +0100

    implementing autoscroll
    works every 2nd message.

--- src/components/chat/MessagesList.js
+++ src/components/chat/MessagesList.js
@@ -1,19 +1,53 @@
-import React from 'react'
+import React, {Component} from 'react'
 import propTypes from 'prop-types'
 import Message from './Message'
 
-const MessagesList = ({ messages }) => (
-    <section id="messages-list">
-        <ul>
-            {messages.map(message => (
-                <Message 
-                    key={message.id}
-                    {...message}
-                />
-            ))}
-        </ul>
-    </section>
-)
+class MessagesList extends Component {
+    constructor(props){
+        super(props)
+        this.state = {scrollPos: 0}
+    }
+    render() {
+        return(
+            <section id="messages-list">
+                <ul>
+                    {this.props.messages.map(message => (
+                        <Message
+                            key={message.id}
+                            {...message}
+                        />
+                    ))}
+                </ul>
+            </section>
+        )
+    }
+    componentWillUpdate(nextProps, nextState) {
+        console.log("LOOK AT THIS FUCKING SHIT: ", document.body.scrollHeight, document.body.clientHeight)
+        this.setState({scrollPos: document.body.scrollHeight - document.body.clientHeight + 66})
+        console.log("I HAZ UPDATE")
+    }
+    shouldComponentUpdate(nextProps, nextState) {
+        console.log(this.props.messages !== nextProps.messages)
+        return this.props.messages !== nextProps.messages
+
+    }
+
+    componentDidUpdate(prevProps, prevState){
+        console.log("LOL")
+        console.log(document.documentElement.scrollTop)
+        console.log(this.state.scrollPos)
+        if(document.documentElement.scrollTop === this.state.scrollPos) {
+            console.log("ROFL")
+            window.scrollTo(0, document.body.scrollHeight)
+        }
+
+    }
+}
+
+
+
+
+
 
 MessagesList.propTypes = {
     message: propTypes.arrayOf(
